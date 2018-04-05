bdcfd39101c8ea95b48d3e3657892a70d15c880e
Zhulinjiuying <554851075@qq.com>
2018-04-06 00:06:35 +0800

    完善log模块

--- middlewares/logger.js
+++ middlewares/logger.js
@@ -1,6 +1,8 @@
 const fs = require('fs')
 const moment = require('moment')
 
+
+// 返回logger的偏函数
 const logger = (path) => {
   return (info) => {
     let date = moment().format('lll')
@@ -10,6 +12,7 @@ const logger = (path) => {
   }
 }
 
+// 初始化log
 const init = () => {
   if (!fs.existsSync('./logs')) {
     fs.mkdirSync('./logs')
@@ -17,6 +20,7 @@ const init = () => {
   global.log = logger(getPath())
 }
 
+// 取得保存地址
 const getPath = () => {
   let date = moment().format('YYYY-MM-DD')
   path = `./logs/${date}.log`
@@ -26,11 +30,10 @@ const getPath = () => {
 module.exports = {
   init: init,
   log: async (ctx, next) => {
-    if (fs.existsSync(getPath())) {
-      log()
-    } else {
+    if (!fs.existsSync(getPath())) {
       init()
     }
+    log(`${ ctx.request.host } ${ ctx.request.method } ${ ctx.request.url }`)
     await next()
   }
 }
